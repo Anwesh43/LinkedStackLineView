130c8d20ad19b1b0d5a01f5ef05c430c822dfe8f
Anwesh43 <anweshthecool0@gmail.com>
2018-08-17 01:42:40 +0530

    importing libraries and defining nodes

--- stacklineview/src/main/java/com/anwesh/uiprojects/stacklineview/SlackLineView.kt
+++ stacklineview/src/main/java/com/anwesh/uiprojects/stacklineview/SlackLineView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.stacklineview
 /**
  * Created by anweshmishra on 17/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int= 5
