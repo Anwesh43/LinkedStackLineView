2e949d285279cb863033ad0c6cd839880a64a012
Anwesh43 <anweshthecool0@gmail.com>
2018-08-17 02:18:24 +0530

    adding StackLineView in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedstacklineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstacklineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedstacklineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.stacklineview.StackLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        StackLineView.create(this)
     }
 }
