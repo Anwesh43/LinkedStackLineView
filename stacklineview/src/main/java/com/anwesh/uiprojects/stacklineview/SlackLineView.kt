485ab5691080ff90445ac5b8f0b9b9fc14018dbe
Anwesh43 <anweshthecool0@gmail.com>
2018-08-17 01:44:38 +0530

    adding StackLineView class

--- stacklineview/src/main/java/com/anwesh/uiprojects/stacklineview/SlackLineView.kt
+++ stacklineview/src/main/java/com/anwesh/uiprojects/stacklineview/SlackLineView.kt
@@ -11,3 +11,21 @@ import android.graphics.Paint
 import android.graphics.Canvas
 
 val nodes : Int= 5
+
+class StackLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
