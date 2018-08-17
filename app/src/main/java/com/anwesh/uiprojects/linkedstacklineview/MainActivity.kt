47b2fd80089f75ca0717737ebb2ab3268d3de5ea
Anwesh43 <anweshthecool0@gmail.com>
2018-08-17 11:12:20 +0530

    adding OnAnimationComplete in Acitivity

--- app/src/main/java/com/anwesh/uiprojects/linkedstacklineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedstacklineview/MainActivity.kt
@@ -3,14 +3,20 @@ package com.anwesh.uiprojects.linkedstacklineview
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import com.anwesh.uiprojects.stacklineview.StackLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        StackLineView.create(this)
+        val view : StackLineView = StackLineView.create(this)
         fullScreen()
+        view.addOnAnimationComplete({createToast("${it + 1} animation completed")}, {createToast("${it + 1} animation is reset")})
+    }
+
+    fun createToast(msg : String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
 
